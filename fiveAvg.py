2ec9f4c29c12cc1098ef1d023c045abfde84b046
Manasi2408 <manasi.bhutra@somaiya.edu>
2025-06-18 11:28:37 +0530

    Initial Commits

---
+++ fiveAvg.py
@@ -0,0 +1,8 @@
+# first five number avg
+
+sum = 0
+for i in range(1,5):
+    sum += i
+
+avg = sum/5
+print(avg)
---
+++ fiveSquare.py
@@ -0,0 +1,4 @@
+# cube or square of first five number
+
+for i in range (1,6):
+    print(i,"=",i*i)
