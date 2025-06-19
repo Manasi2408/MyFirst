10dbe5be84dafff43fc34e71f26cf32543d82d84
Manasi2408 <manasi.bhutra@somaiya.edu>
2025-06-19 14:13:57 +0530

    Initial Commit

---
+++ 10101010.py
@@ -0,0 +1,7 @@
+# display 10101010
+
+for i in range(0,8):
+    if i % 2 == 0:
+        print("1",end=" ")
+    else:
+        print("0",end=" ")
---
+++ 20And50.py
@@ -0,0 +1,4 @@
+# display all the numbers between 20 and 50 including 20 and 50
+
+for i in range(20,51):
+    print(i,end=" ")
